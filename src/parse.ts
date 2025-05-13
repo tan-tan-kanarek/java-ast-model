952214df33490915d05f7a4ddaca7c9cb5246429
Jonathan Amit Kanarek <jonathan.amit.kanarek@walmart.com>
2025-05-13 01:00:12 -0700

    Support generic parse function

--- src/parse.ts
+++ src/parse.ts
@@ -74,8 +74,8 @@ type Sync<S extends String = string> = FilesParser & { read: (file: string) => S
 type Async<S extends String = string> = FilesParser & { readAsync: (file: string) => Promise<S> };
 
 export function parse(sources: string[]): Project;
-export function parse(input: Sync): Project;
-export function parse(input: Async): Promise<Project>;
+export function parse<S extends String = string>(input: Sync<S>): Project;
+export function parse<S extends String = string>(input: Async<S>): Promise<Project>;
 export function parse(
   input: string[] | Sync | Async
 ): Project | Promise<Project> {
